df9135d25d729793a31fdcd91861a25e2088a98b
Léon Mangeat <leon.mangeat@gmail.com>
2017-02-09 11:49:04 +0100

    Ajout communication Azure IoTHub

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var cognitiveApiService = require('../services/cognitiveApiService');
+var Protocol = require('azure-iot-device-amqp').Amqp;
+var Client = require('azure-iot-device').Client;
+var ConnectionString = require('azure-iot-device').ConnectionString;
+var Message = require('azure-iot-device').Message;
+
+var deviceConnectionString = "HostName=CognitiveServiceDemo-IoTHub.azure-devices.net;DeviceId=textAnalyticsDevice;SharedAccessKey=zzNZLmKys/NGPSptMzeYyLdAZac/aAqxTOpA6bJj1Tc=";
+var deviceId = ConnectionString.parse(deviceConnectionString).DeviceId;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -42,11 +49,57 @@ router.post('/', function(req, res, next) {
             );
         }
     ], function (err, langue, iso6391Name, score) {
-        console.log("score :" + score);
+
+        var client = Client.fromConnectionString(deviceConnectionString, Protocol);
+
+        client.open(function (err, result) {
+            if(err){
+                printErrorFor('open')(err);
+            }else{
+                client.on('message', function(msg){
+                    console.log('receive data: ' + msg.getData());
+                    try{
+                        var command = JSON.parse(message.getData());
+                        client.complete(msg, printErrorFor('complete'));
+                    }catch (err){
+                        printErrorFor('parse received message')(err);
+                        client.reject(msg, printErrorFor('reject'));
+                    }
+                });
+
+                client.on('error', function (err) {
+                    printErrorFor('client')(err);
+                    client.close();
+                });
+            }
+
+        });
+
+        var dataToAzure = JSON.stringify({
+            "DeviceID": deviceId,
+            "Message": message,
+            "Langue": langue,
+            "Score": score
+        });
+
+        //Send to Azure
+        client.sendEvent(new Message(dataToAzure), function (err) {
+            printErrorFor('send event');
+            client.close();
+        });
+
         res.render('index', {
             langue: langue,
+            message: message,
             score: score});
     });
 });
+
+// Helper function to print results for an operation
+function printErrorFor(op) {
+    return function printError(err) {
+        if (err) console.log(op + ' error: ' + err.toString());
+    };
+}
 
 module.exports = router;
